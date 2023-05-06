a9114022f6e60eb67098cb665d57fd2073d17dbd
vitor-matheus <vitormatheusob@gmail.com>
2023-05-06 15:13:14 -0300

    pagina-atualizada

--- Componentes/Basicos/Primeiro.js
+++ Componentes/Basicos/Primeiro.js
@@ -1,6 +1,6 @@
 export default function Primeiro (){
 
-    const msg = ' Conteúdo de qualida duvidosa'
+    const msg = ' Exercícios realizados durante o curso sobre React'
 
     return (
 
@@ -8,7 +8,7 @@ export default function Primeiro (){
            
             <h2>Olá,Bem Vindo ao meu Site</h2> 
             <h3>{msg}</h3>
-            <p>Entre e vá a merda </p>  
+            <p>Entre e se divirta</p>  
 
         </div>
     )
